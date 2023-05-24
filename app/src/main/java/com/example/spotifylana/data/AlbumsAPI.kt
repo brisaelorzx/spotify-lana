2135f1f33636e12fdd4568da9321221abb3ef851
brisaelorzx <maiteelorz02@gmail.com>
2023-05-24 16:23:30 -0300

    cambios en AlbumsAPI

--- app/src/main/java/com/example/spotifylana/data/AlbumsAPI.kt
+++ app/src/main/java/com/example/spotifylana/data/AlbumsAPI.kt
@@ -4,12 +4,14 @@ import com.example.spotifylana.model.Album
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface AlbumsAPI {
     @GET("{id}/albums")
     fun getAlbums(
-        @Query("id") artist: String,
-        @Header ("Authorization") token: String
+        @Header ("Authorization") token: String,
+        @Path("id") artist: String
+
     ): Call<ArrayList<Album>>
 }
